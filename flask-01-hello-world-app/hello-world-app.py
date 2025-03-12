e8232de3c969d454a6100e597fcc5f45323ff33a
Seyma4270 <symk180888@gmail.com>
2025-03-12 11:50:37 +0100

    jinja and  hello world app changed

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,18 +1,26 @@
 from flask import Flask 
 app = Flask(__name__)
 @app.route('/')
+def head():
+     return "Hello World Seyma"
 
 
-@app.route('/secondpage')
+@app.route('/second')
+def second():
+     return "this is second Page"
 
 
 @app.route('/third')
+def third():
+     return "this is third Page"
 
 
 @app.route('/fourth/<string:id>')
+def fourth(id):
+   return f'Id of this page {id}'
 
 
 if __name__ == '__main__':
 
-     # app.run(debug=True)
-     app.run(host= '0.0.0.0', port=80)
+     #app.run(debug=True, port=3000)
+     app.run(host= '0.0.0.0', port=8080)
